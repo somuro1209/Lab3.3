576224d608595a28b862112e41eb9bba2e6daaf7
User <angharckleong@hotmail.com>
2017-12-05 15:48:33 +0800

    initial

---
+++ app/src/main/java/com/example/user/lab33/ImportFragment.java
@@ -0,0 +1,43 @@
+package com.example.user.lab33;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImportFragment extends Fragment {
+    private TextView textViewMessage;
+
+
+    public ImportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_import, container, false);
+
+        //Linking UI to program
+        textViewMessage = (TextView)v.findViewById(R.id.textViewMessage);
+        Button buttonCamera = (Button)v.findViewById(R.id.buttonCamera);
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewMessage.setText("Cheese");
+            }
+        });
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+}
